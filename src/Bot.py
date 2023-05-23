f64018081a6bc7b4a308ce16e1c0afabe7b5e4db
szweda <jakubszweda1@gmail.com>
2023-05-23 23:55:22 +0200

    Sending data from modal to config specified channel

--- src/Bot.py
+++ src/Bot.py
@@ -5,22 +5,42 @@ from discord.ext import commands
 f = open("../config.json")
 config = json.loads(f.read())
 
-intents = discord.Intents.default()
-intents.message_content = True
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
 
-@client.event
+class EventModal(discord.ui.Modal, title="Quack"):
+    meetingTitle = discord.ui.TextInput(label="Meeting title", placeholder="reversing?", required=True)
+    description = discord.ui.TextInput(label="Meeting description", placeholder="???", required=True)
+    date = discord.ui.TextInput(label="What time is the meeting?", placeholder="Hope it isn't monday", required=True)
+    room = discord.ui.TextInput(label="Do we have a place to sit?", placeholder="EITI 133?", required=True)
+    beer = discord.ui.TextInput(label="BEER?", placeholder="I hope so", required=True)
+
+    async def on_submit(self, interaction: discord.Interaction):
+        event_channel = bot.get_channel(config['eventChannelId'])
+        event_message = create_event_message(self)
+        await event_channel.send(event_message)
+        await interaction.response.send_message(f"Event {self.meetingTitle} added successfully", ephemeral=True)
+
+
+# Example of an event
+@bot.event
 async def on_ready():
-    print(f'Logged on as {client.user}')
+    print(f'Logged on as {bot.user}')
+    try:
+        synced = await bot.tree.sync()
+        print(f'Synced {len(synced)} commands')
+    except Exception as e:
+        print(e)
 
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    eventChannel = client.get_channel(config['eventChannelId'])
-    await eventChannel.send(f'Message sent by {message.author} : {message.content}')
+@bot.tree.command(name="event")
+@commands.has_role(config['rolePermission'])
+async def event(interaction: discord.Interaction):
+    await interaction.response.send_modal(EventModal())
 
 
-client.run(config['token'])
+def create_event_message(self):
+    return f"@everyone \t{self.meetingTitle}\n{self.description}\n\n\n{self.room},\t{self.date}\n\n{self.beer}\n\n"
+
+
+bot.run(config['token'])
